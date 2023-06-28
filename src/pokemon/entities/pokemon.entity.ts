5e5a06e83eb483bf9dcf7cc99ea930eb8f8e9b21
josue173 <josue.perez04@outlook.com>
2023-06-28 17:57:19 -0600

    Creando esquemas y modelos

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,20 @@
-export class Pokemon {}
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema() // Indicar que es un esquema de base de datos
+export class Pokemon extends Document {
+  // Extiende de Document para ser considerado un documento
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
+// Exportando el Schema
