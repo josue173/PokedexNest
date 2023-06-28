289035f8c19c4675d813c1fe03cbc3de950f4046
josue173 <josue.perez04@outlook.com>
2023-06-28 17:33:51 -0600

    Agregando configurnacion Docker

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v2'); // Prefijo del api
   await app.listen(3000);
 }
 bootstrap();
