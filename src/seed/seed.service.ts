cb36f1df88f0c4324eca678cb2c9991b416b9ec0
josue173 <josue.perez04@outlook.com>
2023-07-04 17:07:17 -0600

    Insertando en DB

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+
+import axios, { AxiosInstance } from 'axios';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios; // Instancia de Axios, dependencia del servicio
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly _pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSEED() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/'); // split para cortar segmentos por '/'
       const no: number = +segments[segments.length - 2];
-      console.log({ name, no });
+      const pokemon = await this._pokemonModel.create({ name, no });
     });
-    return data.results;
+    return 'SEED EXECUTED';
   }
 }
